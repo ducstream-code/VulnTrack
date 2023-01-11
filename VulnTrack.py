accb46a390339daa5ad9ea6abee59dfec24bd38e
aurelien <aurelienkrief@gmail.com>
2023-01-11 14:32:33 +0100

    initial commit

---
+++ VulnTrack.py
@@ -0,0 +1,30 @@
+import os
+import sys
+import argparse
+from bs4 import BeautifulSoup
+
+
+def check_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--techno", "-t",type=str, help="Target to scan",)  # argument qui definira la target
+    parser.add_argument("--os", "-o", help="choose target OS",type=str),
+    parser.add_argument("--version", "-v", help="Display more information when running",type=str),
+    parser.add_argument("--kcve", "-k", help="Search for a know cve",type=str)
+    args = parser.parse_args()
+
+    if args.techno or args.os or args.version or args.kcve:
+        argStart(args)
+    else:
+        menuStart()
+
+
+def argStart(args):
+    print("args")
+
+
+def menuStart():
+    print("menu")
+
+
+if __name__ == '__main__':
+    check_args()
